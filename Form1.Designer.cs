827033bea12b832c8ab5a2eb588a62a21ee9d80a
DingJianchen <jianchending@outlook.com>
2017-05-27 15:45:42 +0800

    adding files

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Blank_TCP_Server
+{
+    partial class tcpform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tcpform));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_setting = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtipport = new System.Windows.Forms.TextBox();
+            this.btn_run = new System.Windows.Forms.Button();
+            this.btn_Send = new System.Windows.Forms.Button();
+            this.txtSendData = new System.Windows.Forms.TextBox();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtTimer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lvClients = new System.Windows.Forms.ListView();
+            this.chstatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chip = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chPort = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvImages = new System.Windows.Forms.ImageList(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lvClients, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 41.56171F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 58.43829F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(377, 397);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 128F));
+            this.tableLayoutPanel2.Controls.Add(this.btn_setting, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel2, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btn_run, 2, 2);
+            this.tableLayoutPanel2.Controls.Add(this.btn_Send, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.txtSendData, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.flowLayoutPanel3, 2, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 37F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(371, 158);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // btn_setting
+            // 
+            this.btn_setting.Location = new System.Drawing.Point(245, 3);
+            this.btn_setting.Name = "btn_setting";
+            this.btn_setting.Size = new System.Drawing.Size(75, 23);
+            this.btn_setting.TabIndex = 1;
+            this.btn_setting.Text = "SET";
+            this.btn_setting.UseVisualStyleBackColor = true;
+            this.btn_setting.Click += new System.EventHandler(this.btn_setting_Click);
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(3, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.txtTimer);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(75, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(164, 34);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.txtipport);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(75, 43);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(164, 34);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // txtipport
+            // 
+            this.txtipport.Location = new System.Drawing.Point(3, 6);
+            this.txtipport.Margin = new System.Windows.Forms.Padding(3, 6, 3, 3);
+            this.txtipport.Name = "txtipport";
+            this.txtipport.Size = new System.Drawing.Size(129, 22);
+            this.txtipport.TabIndex = 0;
+            // 
+            // btn_run
+            // 
+            this.btn_run.Location = new System.Drawing.Point(245, 83);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(75, 23);
+            this.btn_run.TabIndex = 6;
+            this.btn_run.Text = "Start";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // btn_Send
+            // 
+            this.btn_Send.Location = new System.Drawing.Point(3, 83);
+            this.btn_Send.Name = "btn_Send";
+            this.btn_Send.Size = new System.Drawing.Size(66, 23);
+            this.btn_Send.TabIndex = 7;
+            this.btn_Send.Text = "Send";
+            this.btn_Send.UseVisualStyleBackColor = true;
+            this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
+            // 
+            // txtSendData
+            // 
+            this.txtSendData.Location = new System.Drawing.Point(75, 83);
+            this.txtSendData.Name = "txtSendData";
+            this.txtSendData.Size = new System.Drawing.Size(132, 22);
+            this.txtSendData.TabIndex = 8;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(245, 43);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(121, 34);
+            this.flowLayoutPanel3.TabIndex = 9;
+            // 
+            // txtTimer
+            // 
+            this.txtTimer.Location = new System.Drawing.Point(3, 3);
+            this.txtTimer.Name = "txtTimer";
+            this.txtTimer.Size = new System.Drawing.Size(129, 22);
+            this.txtTimer.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 40);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Time";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lvClients
+            // 
+            this.lvClients.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chstatus,
+            this.chip,
+            this.chPort});
+            this.lvClients.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvClients.Location = new System.Drawing.Point(3, 167);
+            this.lvClients.Name = "lvClients";
+            this.lvClients.Size = new System.Drawing.Size(371, 227);
+            this.lvClients.SmallImageList = this.lvImages;
+            this.lvClients.TabIndex = 1;
+            this.lvClients.UseCompatibleStateImageBehavior = false;
+            this.lvClients.View = System.Windows.Forms.View.Details;
+            // 
+            // chstatus
+            // 
+            this.chstatus.Text = "Status";
+            this.chstatus.Width = 83;
+            // 
+            // chip
+            // 
+            this.chip.Text = "IP";
+            this.chip.Width = 140;
+            // 
+            // chPort
+            // 
+            this.chPort.Text = "Port";
+            this.chPort.Width = 63;
+            // 
+            // lvImages
+            // 
+            this.lvImages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("lvImages.ImageStream")));
+            this.lvImages.TransparentColor = System.Drawing.Color.Transparent;
+            this.lvImages.Images.SetKeyName(0, "user-available.ico");
+            this.lvImages.Images.SetKeyName(1, "user-invisible.ico");
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 375);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(377, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tcpform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 397);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "tcpform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BLANK_TCP";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button btn_setting;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.TextBox txtipport;
+        private System.Windows.Forms.Button btn_run;
+        private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.TextBox txtSendData;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.TextBox txtTimer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView lvClients;
+        internal System.Windows.Forms.ImageList lvImages;
+        private System.Windows.Forms.ColumnHeader chstatus;
+        private System.Windows.Forms.ColumnHeader chip;
+        private System.Windows.Forms.ColumnHeader chPort;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using Blank_TCP_Server.Methods;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Blank_TCP_Server
+{
+    public partial class tcpform : Form
+    {
+        private const Int32 DEFAULT_NUM_CONNECTIONS = 40, DEFAULT_BUFFER_SIZE = Int16.MaxValue;
+        SocketListener sl;
+        bool start;
+
+        public tcpform()
+        {
+            InitializeComponent();
+            txtipport.Text = Properties.Settings.Default.ipport;
+            txtTimer.Text = Properties.Settings.Default.beeptime;
+        }
+
+        private void btn_setting_Click(object sender, EventArgs e)
+        {
+            if (txtipport.Text.Length < 3 || txtTimer.Text=="")
+            {
+                MessageBox.Show("請設置正確的IP,PORT,Timer！");
+                return;
+            }
+
+            Properties.Settings.Default.ipport = txtipport.Text;
+            Properties.Settings.Default.beeptime = txtTimer.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            if (txtipport.Text.Length < 3 )
+            {
+                MessageBox.Show("請設置正確的IP和TIME！");
+                return;
+            }
+
+            if (btn_run.Text == "Start")
+            {
+                start = true;
+                btn_run.Text = "Stop";
+                var th = new Thread(run);
+                th.Start();
+                txtipport.Enabled = false;
+                txtTimer.Enabled = false;
+                timer1.Interval = Convert.ToInt32(txtTimer.Text);
+                timer1.Enabled = true;
+            }
+            else
+            {
+                start = false;
+                btn_run.Text = "Start";
+                txtipport.Enabled = true;
+                txtTimer.Enabled = true;
+                timer1.Enabled = false;
+                //lvClients.Clear();
+            }
+            
+        }
+
+        private void run()
+        {
+            try
+            {
+                Int32 port = Convert.ToInt32(txtipport.Text) ;
+                Int32 bufferSize = DEFAULT_BUFFER_SIZE;
+
+                sl = new SocketListener(DEFAULT_NUM_CONNECTIONS, bufferSize);
+                sl.Start(port);
+                string msg = "Server listening on port "+port.ToString()+". Press any key to terminate the server process...\n";
+                Console.ForegroundColor = (ConsoleColor)((60 - 1) % 16);
+                Console.WriteLine(msg);
+                msg = "Timer Invert:" + timer1.Interval;
+                Console.WriteLine(msg);
+                sl.eventlistview += UpdateListView;
+                while (start)
+                {
+
+                }                
+                sl.Stop();
+                Console.WriteLine("Server stoped!\n");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                PrintUsage();
+            }
+            catch (FormatException)
+            {
+                PrintUsage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Usage: SocketAsyncServer <port> [numConnections] [bufferSize].");
+            Console.WriteLine("\t<port> Numeric value for the listening TCP port.");
+            Console.WriteLine("\t[numConnections] Numeric value for the maximum number of incoming connections.");
+            Console.WriteLine("\t[bufferSize] Numeric value for the buffer size of incoming connections.");
+        }
+
+        private void btn_Send_Click(object sender, EventArgs e)
+        {
+            string data;
+            if (txtSendData.Text != string.Empty)
+            {
+                data = txtSendData.Text;
+            }
+            else
+            {
+                data = "hello from server!";
+            }
+            sl.sendfromsocket(data);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                string data = "1";
+                sl.sendfromsocket(data);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        #region UpdateListView From Socket
+        private void UpdateListView(string ip,string status)
+        {
+            //Console.ForegroundColor = (ConsoleColor)((90 - 1) % 16);
+            //Console.WriteLine(ip + "---" + status);
+            //Console.ForegroundColor = (ConsoleColor)((60 - 1) % 16);
+            lvClients.Invoke((MethodInvoker)delegate {
+                if (status.ToUpper() == "C")
+                {
+                    ListViewItem lvi;
+                    lvi = new ListViewItem(" Connected", 0);
+                    var values = ip.Split(':');
+                    lvi.SubItems.Add(values[0]);
+                    lvi.SubItems.Add(values[1]);
+                    lvClients.Items.Add(lvi);
+                }
+                else
+                {
+                    for (int i = lvClients.Items.Count - 1; i >= 0; i--)
+                    {
+                        string s=lvClients.Items[i].SubItems[1].Text+":" + lvClients.Items[i].SubItems[2].Text;
+                        if (s==ip)
+                        {
+                            lvClients.Items[i].Remove();
+                        }
+                    }
+                }
+            });
+        }
+
+        #endregion
+    }
+}
